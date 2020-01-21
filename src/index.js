c987a89f8286f3b04e74ca56ce1e233f1045e6ef
matheus-fls <matheus_fls@yahoo.com.br>
2020-01-21 16:30:21 -0300

    Add Redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from '../components/App';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getID = () => Math.floor(Math.random() * 1000);
+
+const initialState = {
+  books: [
+    {
+      id: getID(),
+      title: 'Wuthering Heights',
+      category: 'Drama',
+    },
+    {
+      id: getID(),
+      title: 'Dracula',
+      category: 'Horror',
+    },
+    {
+      id: getID(),
+      title: 'The Universe in a Nutshell',
+      category: 'Science',
+    },
+  ]
+};
+
+const store = createStore(rootReducer, initialState);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import { combineReducers } from 'redux'
+import books from './book'
+import { createBook, removeBook } from '../actions/index'
 
-export default function index() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export default combineReducers({
+  books,
+  createBook,
+  removeBook,
+})
