8e7ccfcea5afd769a6e8b265b00c5f35fa3f8b47
Diego G <diego.gomes87@gmail.com>
2020-01-21 19:18:28 -0300

    add index and yarn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import App from '../components/App';
+import App from './components/App';
 import rootReducer from './reducers/index';
 
 const getID = () => Math.floor(Math.random() * 1000);
