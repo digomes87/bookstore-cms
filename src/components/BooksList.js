b87e79ae1459f69b4374c5da547abd7ccb872040
Diego G <diego.gomes87@gmail.com>
2020-01-21 13:41:11 -0300

    update bookslist

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux'
 import Book from './Book'
 
-class BookList extends React.Component{
-    
-    render(){
-    return(
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Title</th>
-                        <th>Category</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {books.map(e =>{
-                        <Book id={e.id} title={e.title} category={e.category} />
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
-}
-}
-
-const mapStateToProps = (state, ownProps)=>{
-    return {
-        books: state.books
-    }
-}
+const BookList = ({books}) => ( 
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                {books.map(e =>{
+                    <Book id={e.id} title={e.title} category={e.category} />
+                })}
+            </tbody>
+        </table>
+    </div>
+)
 
 export default BookList
