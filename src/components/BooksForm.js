1d1d8095a6dba68b4be4067ca95dd5dcd5dda14e
Diego G <diego.gomes87@gmail.com>
2020-01-21 18:18:51 -0300

    error in handlerSubmit

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -2,17 +2,40 @@ import React from 'react';
 
 const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <div>
-    <h2>BookList</h2>
-    <form>
-      <input type="text" name="title" />
-      {bookCategories.map((p, i) => (
-				<select key={i}>{p}</select> /* eslint-disable-line */
-      ))}
-      <button type="submit">Save</button>
-    </form>
-  </div>
-);
+class BooksForm extends React.Component { 
+  constructor(props){
+    super(props);
+    this.state = {
+      title: '',
+      category: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange(event){
+
+    this.setState({
+      title: event.target.value 
+    })
+  }
+
+
+  handleSubmit(){
+    event.preventDefault();
+  }
+
+  render() {
+    <div>
+      <h2>BookList</h2>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="title" />
+        {bookCategories.map((p, i) => (
+          <select key={i}>{p}</select> /* eslint-disable-line */
+        ))}
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  }
+}
 export default BooksForm;
