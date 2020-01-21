c50ec035e5584562886eec2d13080bb99ec15e4e
Diego G <diego.gomes87@gmail.com>
2020-01-21 14:29:24 -0300

    add list of categories

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,1 +1,22 @@
+import React from 'react';
 
+const bookCategories = [ 
+    "Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"
+];
+
+
+
+const BooksForm = () => {(
+    <div>
+        <h2>BookList</h2>
+        <form>
+            <input type="text" name="title"/>
+            {bookCategories.map((p, i) => (
+                <select key={i}>{p}</select>
+            ))}
+            <button type="submit">Save</button>
+        </form>
+    </div>
+)};
+
+export default BooksForm
