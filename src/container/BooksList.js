631bc3c9ca4702e173448076b2ba9f01cf4f893a
matheus-fls <matheus_fls@yahoo.com.br>
2020-01-22 14:24:41 -0300

    Fix linter errors

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -1,41 +1,47 @@
 import React from 'react';
-import Book from '../components/Book';
 import { connect } from 'react-redux';
+import Book from '../components/Book';
+import PropTypes from 'prop-types';
 
 class BooksList extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    const { items } = this.props;
     this.state = {
-      books: this.props.items,
+      books: items,
     };
-  };
-  
+  }
+
   render() {
-    return(
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.books.map(e => (
-            <Book id={e.id} title={e.title} category={e.category} key={e.id} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-    )
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.books.map(e => (
+              <Book id={e.id} title={e.title} category={e.category} key={e.id} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   } 
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   return {
     items: state.books,
-  }
-}
+  };
+};
+
+BooksList.propTypes = {
+  items: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(BooksList);
