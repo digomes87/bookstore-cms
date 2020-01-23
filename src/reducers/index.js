de8053a7bfb362cb6e1e56cdf1c659d8cc1fc559
Diego Go <diego.gomes87@gmail.com>
2020-01-23 13:27:45 -0300

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,21 @@
-import { combineReducers } from 'redux';
-import books from './book';
+const getID = () => Math.floor(Math.random() * 1000);
 
-export default combineReducers({
-  books,
-});
+const reducers = (state = [], action) => {
+  const id = getID();
+  switch (action.type) {
+    case 'CREATE_BOOK':
+      return [...state, {
+        id,
+        title: action.book.title,
+        category: action.book.category,
+      }];
+
+    case 'REMOVE_BOOK':
+      return state.filter(j => j.id !== action.id);
+
+    default:
+      return state;
+  }
+};
+
+export default reducers;
