8c2c3aaec1fd2d92d1f86e1f30afa20c1fd0e33b
Diego Go <diego.gomes87@gmail.com>
2020-01-23 13:25:46 -0300

    update BookForm

--- src/container/BooksForm.js
+++ src/container/BooksForm.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBook } from '../actions/index';
 
 class BooksForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '', /* eslint-disable-line */
-      category: 'Action', /* eslint-disable-line */
+      title: '',
+      category: 'Action',
     };
+  }
+
+  handleChange = (event) => (
+    event.target.tagName === 'INPUT'
+      ? this.setState({ title: event.target.value })
+      : this.setState({ category: event.target.value })
+  )
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.createBook(this.state); /* eslint-disable-line */
   }
 
   render() {
     const bookCategories = ['Action', 'Biography', 'Drama', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-
+    const { title, category } = this.state;
     return (
       <div>
         <h2>Add Book</h2>
-        <form>
-          <input type="text" name="title" />
-          <select>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="title" onChange={this.handleChange} value={title} />
+          <select onChange={this.handleChange} value={category}>
             {bookCategories.map((p, i) => (
               <option key={i}>{p}</option> /* eslint-disable-line */
             ))}
@@ -29,4 +44,12 @@ class BooksForm extends React.Component {
   }
 }
 
-export default BooksForm;
+const mapDispatchToProps = dispatch => ({
+  createBook: book => dispatch(createBook(book)),
+});
+
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(BooksForm);
