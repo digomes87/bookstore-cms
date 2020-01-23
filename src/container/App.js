22b92ee7cad96715cb7ff20a8a41ea10318c4b98
Diego G <diego.gomes87@gmail.com>
2020-01-23 12:34:20 -0300

    update App imports

--- src/container/App.js
+++ src/container/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BooksList from './BooksList';
-import BooksForm from './BooksForm';
+import BooksList from './BooksList.js';
+import BooksForm from './BooksForm.js';
 
 function App(store) {
   return (
